233fde8e89d01b220deff00eef897787d040e674
Lucas Vogel <contact@vogelino.com>
2022-11-04 21:20:38 +0100

    Cleaning stuff

--- macos/dock.sh
+++ macos/dock.sh
@@ -2,10 +2,15 @@
 
 dockutil --no-restart --remove all
 dockutil --no-restart --add "/System/Applications/System Preferences.app"
+dockutil --no-restart --add "/System/Applications/Mail.app"
+dockutil --no-restart --add "/System/Applications/Code.app"
 dockutil --no-restart --add "/System/Applications/iTerm.app"
-dockutil --no-restart --add "/System/Applications/Calendar.app"
-dockutil --no-restart --add "/Applications/Google Chrome.app"
-dockutil --no-restart --add "/System/Applications/Todoist.app"
+dockutil --no-restart --add "/System/Applications/Slack.app"
+dockutil --no-restart --add "/Applications/Opera GX.app"
+dockutil --no-restart --add "/System/Applications/1Password 7.app"
+dockutil --no-restart --add "/System/Applications/DeepL.app"
+dockutil --no-restart --add "/System/Applications/Notion.app"
+dockutil --no-restart --add "/System/Applications/Signal.app"
 
 killall Dock
 
