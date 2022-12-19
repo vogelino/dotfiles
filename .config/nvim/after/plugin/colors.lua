259b527f68099d6ccc4ad2d2cf948b97ba9225b6
Lucas Vogel <contact@vogelino.com>
2022-12-19 22:40:28 +0100

    Update configs

--- .config/nvim/after/plugin/colors.lua
+++ .config/nvim/after/plugin/colors.lua
@@ -1,5 +1,10 @@
 function ColorMyPencils(color)
   color = color or "tokyonight-night"
+  require("lualine").setup({
+    options = {
+      theme = "tokyonight-night"
+    }
+  })
   vim.cmd.colorscheme(color)
 end
 
