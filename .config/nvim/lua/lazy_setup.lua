b0ee151d6657a57f449ba3688aa080f8b961d89e
Lucas Vogel <lucas@deepjudge.ai>
2026-05-31 19:14:02 +0200

    Remove octo.nvim

--- .config/nvim/lua/lazy_setup.lua
+++ .config/nvim/lua/lazy_setup.lua
@@ -11,8 +11,7 @@ require("lazy").setup({
       update_notifications = true, -- Enable/disable notification about running `:Lazy update` twice to update pinned plugins
     },
   },
-  { import = "community" },
-  { import = "plugins" },
+{ import = "plugins" },
 } --[[@as LazySpec]], {
   -- Configure any other `lazy.nvim` configuration options here
   install = { colorscheme = { "astrotheme", "habamax" } },
